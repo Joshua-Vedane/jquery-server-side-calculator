6488fe5196f9652677e08d0f635047a4815580f8
Joshua Vedane <jvedane@gmail.com>
2020-12-18 18:02:22 -0600

    created handleSubmit and calcObject POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,8 +4,8 @@ function handleReady(){
     console.log('JQ');
 
     $('.operator').on('click', handleOperator);
-    // $('#submit').on('click', handleSubmit())
-    // $('#clear').on('click', handleClear())
+    $('#submit').on('click', handleSubmit);
+    $('#clear').on('click', handleClear);
 }
 
 let calcObject = {};
@@ -15,4 +15,25 @@ function handleOperator (){
     let operator = $(this).data('operator');
     console.log(operator);
     calcObject.operator = operator;
+}
+
+function handleSubmit(){
+    //add in the numbers to the calcObject
+    let number1 = $('#firstNumIn').val();
+    let number2 = $('#secondNumIn').val();
+    calcObject.numOne = number1;
+    calcObject.numTwo = number2;
+
+    // POST calcObject to server
+    $.ajax({
+        url : '/data',
+        type : 'POST',
+        data : calcObject
+    }).then(function)(response){
+        console.log(response);
+        //GO GET THE CALCULATIONS!!!
+    }
+
+    //clear calcObject? or in clear is fine too 
+    //reset inputs (in clear as well?)
 }
