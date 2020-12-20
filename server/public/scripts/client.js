7624c5e091ad31c5b74a2c510f4f34c9614a57d5
Joshua Vedane <jvedane@gmail.com>
2020-12-20 11:52:33 -0600

    cleaned up javascript files and added some styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,37 +1,29 @@
 $(document).ready(handleReady);
 
 function handleReady(){
-    console.log('JQ');
-
+    //click listeners
     $('.operator').on('click', handleOperator);
     $('#submit').on('click', handleSubmit);
-    // $('#clear').on('click', handleClear);
+    $('#clear').on('click', handleClear);
 }
 
-// should have syntax of {
-//     operator : '*',
-//     num1: 'number',
-//     num2 : 'number'
-// }
 let calcObject = {};
 
 function handleOperator (){
     delete calcObject.operator;
     let operator = $(this).data('operator'); 
-    // operator = 'add'
-    console.log(operator);
+    // console.log(operator);
     calcObject.operator = operator;
-    console.log(calcObject);
+    // console.log(calcObject);
 }
 
+
 function handleSubmit(){
-    //add in the numbers to the calcObject
-    //clear the calcObject numbers here
     let number1 = $('#firstNumIn').val();
     let number2 = $('#secondNumIn').val();
     calcObject.num1 = number1;
     calcObject.num2 = number2;
-    console.log(calcObject);
+    // console.log(calcObject);
 
     // POST calcObject to server
     $.ajax({
@@ -39,26 +31,16 @@ function handleSubmit(){
         type : 'POST',
         data : calcObject
     }).then(function(response){
-        console.log(response);
-        //get the last calculation. 
-        //Get all calculations(at this point, lastCalcObject/Result still exists)
-        getLastCalculation();
+        // console.log(response);
+        //get results 
         getCalculations();
-        //clear input fields
-        $('.numberIn').val('');
     })
     
 }
 
-//get last calculation and render
-function getLastCalculation(){
-    $.ajax({
-        url : '/last',
-        type : 'GET'
-    }).then(function(response){
-        console.log(response);
-        $('#lastResultOut').text(response.result);
-    });
+//clear input fields
+function handleClear(){
+    $('.numberIn').val('');
 }
 
 //GET calculations and render to DOM
@@ -67,19 +49,11 @@ function getCalculations(){
         url : '/data',
         type : 'GET'
     }).then(function(response){
-        console.log(response);
+        // console.log(response);
         $('#resultsOut').empty();
         for(let item of response){
-            // let itemSign = '';
-            // if(item.operator === 'add'){
-            //     itemSign = '+';
-            // }else if(item.operator === 'sub'){
-            //     itemSign = '-';
-            // }else if (item.operator === 'mult'){
-            //     itemSign = 'x'
-            // }else if(item.operator === 'divide'){
-            //     itemSign = '/'
             $('#resultsOut').append(`<li>${item.num1} ${item.operator} ${item.num2} = ${item.result}</li>`)
+            $('#lastResultOut').text(item.result);
             }
         })
     
--- server/server.js
+++ server/server.js
@@ -5,84 +5,56 @@ const app = express();
 
 
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Store Data and functions here
 const receivedCalcs = [];
-let lastCalcResult = {};
+
 let lastCalcObject = {};
 
-function calculateResults(newCalc){
+function calculateResults(newCalc) {
     lastCalcObject = {};
-    lastCalcResult = {};    
-    //loop over new object
-    //newCalc not iterable. remove for loop and just do condos
-    //bring lastCalcResult into this function. add key:values to it. then, push it into receivedCalcs. 
-        console.log(`newCalc in CalculateResults:`,newCalc);
-        if (newCalc.operator === '+'){
-            let newAddCalc = Number(newCalc.num1) + Number(newCalc.num2);
-            lastCalcResult.result = Number(newAddCalc);
-            lastCalcObject.result = newAddCalc;
-        } else if(newCalc.operator === '-'){
-            let newSubCalc = Number(newCalc.num1) - Number(newCalc.num2);
-            lastCalcResult.result = Number(newSubCalc);
-            lastCalcObject.result = newSubCalc;
-        } else if(newCalc.operator === '*'){
-            let newMultCalc = Number(newCalc.num1) * Number(newCalc.num2);
-            lastCalcResult.result = Number(newMultCalc);
-            lastCalcObject.result = newMultCalc;
-        } else if(newCalc.operator === '/'){
-            let newDivideCalc = Number(newCalc.num1) / Number(newCalc.num2);
-            lastCalcResult.result = Number(newDivideCalc);
-            lastCalcObject.result = newDivideCalc;
-        }else{
-            console.log('something wrong');
-        }
+    // console.log(`newCalc in CalculateResults:`,newCalc);
+    if (newCalc.operator === '+') {
+        let newAddCalc = Number(newCalc.num1) + Number(newCalc.num2);
+        lastCalcObject.result = newAddCalc;
+    } else if (newCalc.operator === '-') {
+        let newSubCalc = Number(newCalc.num1) - Number(newCalc.num2);
+        lastCalcObject.result = newSubCalc;
+    } else if (newCalc.operator === '*') {
+        let newMultCalc = Number(newCalc.num1) * Number(newCalc.num2);
+        lastCalcObject.result = newMultCalc;
+    } else if (newCalc.operator === '/') {
+        let newDivideCalc = Number(newCalc.num1) / Number(newCalc.num2);
+        lastCalcObject.result = newDivideCalc;
+    } else {
+        console.log('something wrong');
+    }
     lastCalcObject.num1 = newCalc.num1;
     lastCalcObject.num2 = newCalc.num2;
     lastCalcObject.operator = newCalc.operator;
-
-   
 }
-
-
 //End DATA AND FUNCTIONS
 
-
 //GET REQUESTS
-    //get last calc 
-app.get('/last', (req, res) => {
-    console.log('get requested /last');
-    res.send(lastCalcResult);
-    console.log(`GET response /LAST`,lastCalcResult);
-});
-
-    //get all calcs
-
-app.get('/data', (req,res) =>{
-    console.log('get requested /data');
+//get all calcs
+app.get('/data', (req, res) => {
+    // console.log('get requested /data', receivedCalcs);
     res.send(receivedCalcs);
-    console.log(receivedCalcs);
 })
 
 //POST REQUESTS
 // post calcObject
-app.post('/data', (req,res) => {
+app.post('/data', (req, res) => {
     let newCalc = req.body;
-    console.log('POST REQ /post', newCalc);
-    //no need to clear out lastCalcObject/Results b/c reassigning in function. 
-    
-    //here we'll want to call a new function that takes in 'newCalc' as a parameter and do our calculations there. The result of which will be put into the receivedCalcs array or something similar
+    // console.log('POST REQ /post', newCalc);
     calculateResults(newCalc);
-    //push lastCalcObject to receivedCalcs?
-    console.log(`lastCalcObject is;`, lastCalcObject);
-    console.log(`lastCalcResult is: `,lastCalcResult.result);
+    // console.log(`lastCalcObject is;`, lastCalcObject);
     receivedCalcs.push(lastCalcObject);
-    console.log(receivedCalcs);
+    // console.log(receivedCalcs);
     res.sendStatus(201);
 
 })
-
 
 //ZOOM ZOOM
 const PORT = 5000;
